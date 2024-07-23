b531b74232d632f9bda297163527c1b1f8b4d8de
Cats Juice <wp1298554944@hotmail.com>
2024-07-23 09:19:11 +0800

    Bump version to 0.1.2

--- src/react/index.d.ts
+++ src/react/index.d.ts
@@ -1,6 +1,6 @@
 export * from "./type";
 export declare const MasonryFlow: {
-    Root: ({ gap, width, children, scrollable, transitionDuration, transitionTiming, onScroll: propsOnScroll, style, ...attrs }: import("./type").MasonryFlowProps) => import("react").JSX.Element;
+    Root: ({ gap, width, children, scrollable, transitionDuration, transitionTiming, onScroll: propsOnScroll, style, locationMode, ...attrs }: import("./type").MasonryFlowProps) => import("react").JSX.Element;
     Item: ({ children, style, height, index, ...attrs }: import("./type").MasonryFlowItemProps) => import("react").JSX.Element;
     Footer: ({ children }: {
         children?: import("react").ReactNode;
